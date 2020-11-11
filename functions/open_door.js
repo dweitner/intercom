a4b404f09f528d9ab890c69ba67bf693c8bedc72
Doug Weitner <dweitner@squarespace.com>
2020-11-11 16:25:50 -0500

    readd fetch import

--- functions/open_door.js
+++ functions/open_door.js
@@ -1,11 +1,11 @@
-// const fetch = require("node-fetch");
+import fetch from "node-fetch";
 // const API_ENDPOINT = "http://74.71.87.249/";
 
-exports.handler = async (event, context) => {
+export async function handler(event, context) {
     return fetch("http://74.71.87.249/")
       .then((data) => ({
         statusCode: 200,
         body: data,
       }))
       .catch((error) => ({ statusCode: 500, body: String(error) }));
-};
+}
