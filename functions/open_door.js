d8b99d8bb22ffd533e6972e41966d834c48b1e51
Doug Weitner <dweitner@squarespace.com>
2020-11-11 16:19:18 -0500

    dummy out function

--- functions/open_door.js
+++ functions/open_door.js
@@ -1,11 +1,15 @@
-const fetch = require('node-fetch');
-const API_ENDPOINT = "http://74.71.87.249/";
+// const fetch = require("node-fetch");
+// const API_ENDPOINT = "http://74.71.87.249/";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT)
-    .then((data) => ({
-      statusCode: 200,
-      body: data,
-    }))
-    .catch((error) => ({ statusCode: 500, body: String(error) }));
+  return {
+    statusCode: 200,
+    body: "Hi from function",
+  };
+  //   return fetch(API_ENDPOINT)
+  //     .then((data) => ({
+  //       statusCode: 200,
+  //       body: data,
+  //     }))
+  //     .catch((error) => ({ statusCode: 500, body: String(error) }));
 };
